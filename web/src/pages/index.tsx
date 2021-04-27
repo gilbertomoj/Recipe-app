f95e3c6afef9e446a44ea1b137116674f01133a0
Mario Leandro <marioleandro34@gmail.com>
2021-04-27 13:49:25 -0300

    add recipe card template

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,8 +1,81 @@
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className={styles.homepage}>
+      <div className={styles.recipes}>
+        <h2>Receitas</h2>
+
+        <ul>
+          <li>
+            <div className={styles.cardContainer}>
+              <div>
+                <img src="https://i.pinimg.com/736x/b1/0b/f3/b10bf36c6d6f4cc32819803c801bbec8.jpg" alt=""/>
+                <h3>Bolo de brigadeiro</h3>
+              </div>
+              <p>Nível: Iniciante</p>
+              <p>Bolo de chocolate com recheio de beijinho e doce de leite e cobertura de creme de chocolate meio amargo, coco ralado, bala de coco e pedacinhos de doce de leite.</p>
+            </div>
+          </li>
+          <li>
+            <div className={styles.cardContainer}>
+              <div>
+                <img src="https://i.pinimg.com/736x/b1/0b/f3/b10bf36c6d6f4cc32819803c801bbec8.jpg" alt=""/>
+                <h3>Bolo de brigadeiro</h3>
+              </div>
+              <p>Nível: Iniciante</p>
+              <p>Bolo de chocolate com recheio de beijinho e doce de leite e cobertura de creme de chocolate meio amargo, coco ralado, bala de coco e pedacinhos de doce de leite.</p>
+            </div>
+          </li>
+          <li>
+            <div className={styles.cardContainer}>
+              <div>
+                <img src="https://i.pinimg.com/736x/b1/0b/f3/b10bf36c6d6f4cc32819803c801bbec8.jpg" alt=""/>
+                <h3>Bolo de brigadeiro</h3>
+              </div>
+              <p>Nível: Iniciante</p>
+              <p>Bolo de chocolate com recheio de beijinho e doce de leite e cobertura de creme de chocolate meio amargo, coco ralado, bala de coco e pedacinhos de doce de leite.</p>
+            </div>
+          </li>
+          <li>
+            <div className={styles.cardContainer}>
+              <div>
+                <img src="https://i.pinimg.com/736x/b1/0b/f3/b10bf36c6d6f4cc32819803c801bbec8.jpg" alt=""/>
+                <h3>Bolo de brigadeiro</h3>
+              </div>
+              <p>Nível: Iniciante</p>
+              <p>Bolo de chocolate com recheio de beijinho e doce de leite e cobertura de creme de chocolate meio amargo, coco ralado, bala de coco e pedacinhos de doce de leite.</p>
+            </div>
+          </li>
+          <li>
+            <div className={styles.cardContainer}>
+              <div>
+                <img src="https://i.pinimg.com/736x/b1/0b/f3/b10bf36c6d6f4cc32819803c801bbec8.jpg" alt=""/>
+                <h3>Bolo de brigadeiro</h3>
+              </div>
+              <p>Nível: Iniciante</p>
+              <p>Bolo de chocolate com recheio de beijinho e doce de leite e cobertura de creme de chocolate meio amargo, coco ralado, bala de coco e pedacinhos de doce de leite.</p>
+            </div>
+          </li>
+          <li>
+            <div className={styles.cardContainer}>
+              <div>
+                <img src="https://i.pinimg.com/736x/b1/0b/f3/b10bf36c6d6f4cc32819803c801bbec8.jpg" alt=""/>
+                <h3>Bolo de brigadeiro</h3>
+              </div>
+              <p>Nível: Iniciante</p>
+              <p>Bolo de chocolate com recheio de beijinho e doce de leite e cobertura de creme de chocolate meio amargo, coco ralado, bala de coco e pedacinhos de doce de leite.</p>
+            </div>
+          </li>
+          
+          
+          {/* {latestEpisodes.map((episode, index) => (
+            <li key={episode.id}>
+              
+            </li>
+          ))} */}
+        </ul>
+      </div>
     </div>
   )
 }
